d4cb09b61ed30b1369d58593cc8b501514b9198f
vamshi0129 <151362743+vamshi0129@users.noreply.github.com>
2025-10-14 21:07:58 +0530

    Add files via upload

--- src/LiveWorkshop.jsx
+++ src/LiveWorkshop.jsx
@@ -21,16 +21,26 @@ export default function LiveWorkshop() {
       {/* <h1>Stay tuned for the updates ! </h1> */}
       <div className="overlay">
         <div className="card">
-          <img
-            src="/images/tissue.jpg"
+          <video 
+            autoPlay
+            loop
+            muted
+            playsInline
+            src="/images/diya_poster.mp4"
             alt="Dance Workshop"
             className="workshop-img"
           />
 
+          {/* <img
+            src="/images/diya.png"
+            alt="Dance Workshop"
+            className="workshop-img"
+          /> */}
+
           <div className="details">
-            <p className="title">Tissue Art Workshop</p>
-            <p>on <strong>12th October</strong></p>
-            <p>At Belloso Cafe , Uppal, Hyderabad</p>
+            <p className="title">Diya Making Workshop</p>
+            <p>on <strong>19th October</strong></p>
+            <p>At Paper Crown Cafe , Banjara Hills , Hyderabad</p>
             <p>@11AM </p>
 
             <button className="btn" onClick={() => setShowForm(true)}>
@@ -42,8 +52,8 @@ export default function LiveWorkshop() {
             <div className="form-container">
               <h2>Pass Booking Form</h2>
               <div className="iframe-wrapper">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8be_jpUuoruslpgnGBo5RhspXBY1skd56mfUcwnueutb8-Q/viewform?embedded=true" width="640" height="2137" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-              </div>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSelIk0iW_jnL312fOUTs3LJYNpwtw3NXhEMHTLHu9Jrb-Punw/viewform?embedded=true" width="640" height="2137" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div>
             </div>
           )}
         </div>
