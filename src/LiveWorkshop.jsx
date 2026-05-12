23e0a92a9b4248001157902b6df88ec02c37ba34
vamshi0129 <151362743+vamshi0129@users.noreply.github.com>
2026-05-12 12:21:19 +0530

    Add files via upload

--- src/LiveWorkshop.jsx
+++ src/LiveWorkshop.jsx
@@ -18,24 +18,16 @@ export default function LiveWorkshop() {
       />
 
       {/* Content overlay */}
-      <h1>Stay tuned for the updates ! </h1>
-      {/* <div className="overlay">
+      {/* <h1>Stay tuned for the updates ! </h1> */}
+      <div className="overlay">
         <div className="card">
-          <video 
-            autoPlay
-            loop
-            muted
-            playsInline
-            src="/images/diya_poster.mp4"
-            alt="Dance Workshop"
-            className="workshop-img"
-          />
+          <img src="/images/kitsugi.png" alt="Kintsugi Art Workshop" className="workshop-img" />
 
           <div className="details">
-            <p className="title">Diya Making Workshop</p>
-            <p>on <strong>19th October</strong></p>
-            <p>At Paper Crown Cafe , Banjara Hills , Hyderabad</p>
-            <p>@11AM </p>
+            <p className="title">Kintsugi Art Workshop</p>
+            <p>on <strong>17th May</strong></p>
+            <p>At Belloso cafe , Uppal , Hyderabad</p>
+            <p>@11AM - 2PM </p>
 
             <button className="btn" onClick={() => setShowForm(true)}>
               Buy Pass
@@ -46,12 +38,12 @@ export default function LiveWorkshop() {
             <div className="form-container">
               <h2>Pass Booking Form</h2>
               <div className="iframe-wrapper">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSelIk0iW_jnL312fOUTs3LJYNpwtw3NXhEMHTLHu9Jrb-Punw/viewform?embedded=true" width="640" height="2137" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScjxAN7YGQMTH6SSk5DCUyjKOf6qYVxcHqe30MzG3TuBUfhhA/viewform?embedded=true" width="640" height="1653" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </div>
           )}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
